9c532a18ce50b792aace93a5fe77e6da61789f33
Kwinner <chenkai.c.k@hotmail.com>
2018-07-04 05:28:18 +0800

    整理代码

--- image_rec.py
+++ image_rec.py
@@ -3,14 +3,14 @@
 
 import requests, base64, json
 
-def recognize(image, access_token, flag=1):
+def recognize(image, access_token, flag='content'):
     '''
        返回图片识别结果的字典。
        参数解析：
        -image:str,图片的绝对路径。
        -access_token:str,需要根据百度AI开放平台提供的API Key和Secret Key得到。
                      可以使用tokens中get_token.py中的get_token()方法。
-       -flag: 1 or 2, 与access_token对应，当access_token为检测类型时为1，检测内容时为2.
+       -flag: type, content or position. type时为识别主题类型，content为识别内容，position识别主题位置。
 
         返回结果的键解释：
         log_id: int,每个结果唯一的id，
@@ -20,8 +20,9 @@ def recognize(image, access_token, flag=1):
         +root：识别结果的分类，上层标签，
         +keyword：图片中物体或场景名。
     '''
-    base_url_1 = 'https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general'
-    base_url_2 = 'https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic'
+    base_url_t = 'https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general'
+    base_url_c = 'https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic'
+    base_url_p = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/object_detect'
     payload = {
         'access_token':access_token,
     }
@@ -30,10 +31,12 @@ def recognize(image, access_token, flag=1):
     }
     with open(image, 'br') as f:
         obj = base64.b64encode(f.read())
-    if flag == 1:
-        resp = requests.post(url=base_url_1, params=payload, headers=header, data={'image':obj})
-    elif flag == 2:
-        resp = requests.post(url=base_url_2, params=payload, headers=header, data={'image':obj})
+    if flag == 'content':
+        resp = requests.post(url=base_url_c, params=payload, headers=header, data={'image':obj})
+    elif flag == 'type':
+        resp = requests.post(url=base_url_t, params=payload, headers=header, data={'image':obj})
+    elif flag == 'position':
+        resp = requests.post(url=base_url_p, params=payload, headers=header, data={'image':obj})
     resp.encoding = resp.apparent_encoding
     jsn = resp.text
     dic = json.loads(jsn)
@@ -41,6 +44,11 @@ def recognize(image, access_token, flag=1):
 
 if __name__ == '__main__':
     from tokens.get_token import get_token
-    access_token = get_token(ak='hgO2AIK9L5s20coavGHBRyQI',sk='gK5AQOzUgtirD4EPlSlbuRscbKVnWZMU')['access_token']
-    dic = recognize(r'C:\Users\kkche\Pictures\003bsgbmgy6R6efoOr1c3.png',access_token, flag=2)
+    ak = input('输入你的API Key：')
+    sk = input('输入你的Secret Key：')
+    file = input('需要识别的图片路径：')
+    flag = input('识别类型（type：主题类型，content：内容，position：主体位置）：')
+    access_token = get_token(ak=ak,sk=sk)['access_token']
+    dic = recognize(r'%s' % file, access_token, flag=flag)
     print(dic)
+    input('按任意键退出！')
--- tokens/get_token.py
+++ tokens/get_token.py
@@ -6,7 +6,7 @@ import os
 import pickle, json
 import time
 
-file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'access_token_%s.txt')
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'access_token.pkl')
 
 def _refresh_token(ak, sk):  # 请求token，返回字典
     '''ak:API_Key,
@@ -27,7 +27,7 @@ def _refresh_token(ak, sk):  # 请求token，返回字典
     dic.update({'client_id':ak, 'client_secret':sk})
     return dic
 
-def get_token(ak, sk, flag='content'):  # 试图从文件读取token，过期或不存在或密钥不匹配刷新后，再返回字典
+def get_token(ak, sk):  # 试图从文件读取token，过期或不存在或密钥不匹配刷新后，再返回字典
     '''用于获取ak=api_key,sk=secret_key的token，返回的是一个字典。ak,sk在百度开放AI平台账户生成。
        用于检测类型时flag为type，用于检测内容时flag为content。
        结果字典包含:
@@ -37,17 +37,16 @@ def get_token(ak, sk, flag='content'):  # 试图从文件读取token，过期或
        "session_key":str,
        "access_token:str,用于api,
        "session_secret":str
-       "flag":str,token的用途，用于识别类型时为“type”，用于识别内容是为“content”。
        '''
-    file = file_path % flag
-    if os.path.isfile(file) and os.path.getsize(file)>0:
-        with open(file, '+rb') as f:
+    
+    if os.path.isfile(file_path) and os.path.getsize(file_path)>0:
+        with open(file_path, '+rb') as f:
             dic = pickle.load(f)
             if dic['client_id'] != ak or dic['client_secret'] != sk:
                 dic = _refresh_token(ak, sk)
                 pickle.dump(dic, f)
                 return dic
-            elif time.time()-os.path.getatime(file) > dic.get('expires_in'):
+            elif time.time()-os.path.getatime(file_path) > dic.get('expires_in'):
                 dic = _refresh_token(ak, sk)
                 pickle.dump(dic, f)
                 return dic         
@@ -55,10 +54,13 @@ def get_token(ak, sk, flag='content'):  # 试图从文件读取token，过期或
                 return dic
     else:
         dic = _refresh_token(ak, sk)
-        with open(file, 'wb') as f:
+        with open(file_path, 'wb') as f:
             pickle.dump(dic, f)
         return dic
 
 if __name__ == '__main__':
-    dic = get_token(ak='hgO2AIK9L5s20coavGHBRyQI', sk='gK5AQOzUgtirD4EPlSlbuRscbKVnWZMU')
-    print(dic['access_token'])
+    ak = input('输入API Key：')
+    sk = input('输入Secret Key:')
+    dic = get_token(ak=ak, sk=sk)
+    print('你的access_token是：%s' % dic['access_token'])
+    input('任意键退出！')
